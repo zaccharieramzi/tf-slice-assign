124db15948285b2fb621b06e702664a13437e7c7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-04 13:49:10 +0200

    added mechanism to list the indexing positions and the corresponding range

--- tf_slice_assign.py
+++ tf_slice_assign.py
@@ -13,9 +13,26 @@ def slice_assign(sliced_tensor, assigned_tensor, *slice_args):
         or slice.
     """
     n_dims = len(tf.shape(sliced_tensor))
+    n_slices = len(slice_args)
     dims_to_index = []
-    for i_dim, slice in enumerate(slice_args):
-        pass
+    corresponding_ranges = []
+    three_dots = False
+    for i_dim, slice_spec in enumerate(slice_args):
+        if isinstance(slice_spec, str):
+            if slice_spec == ':':
+                continue
+            elif slice_spec == '...':
+                three_dots = True
+            else:
+                raise ValueError('Slices must be :, ..., or slice object.')
+        else:
+            start, stop, step = slice_spec.start, slice_spec.stop, slice_spec.step
+            corresponding_range = tf.range(start, stop, step)
+            if three_dots:
+                dims_to_index.append(i_dim + (n_dims - n_slices))
+            else:
+                dims_to_index.append(i_dim)
+            corresponding_ranges.append(corresponding_range)
     sliced_tensor_reshaped = sliced_tensor
     update_indices = []
     assigned_tensor_reshaped = assigned_tensor
