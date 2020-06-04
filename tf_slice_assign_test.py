809ec45838bc2877eaa3291adc7b0eb385da7ce7
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-06-04 16:55:52 +0200

    added some more test cases regarding ellipsis

--- tf_slice_assign_test.py
+++ tf_slice_assign_test.py
@@ -16,10 +16,16 @@ def handle_slice_arg(slice_arg):
 
 @pytest.mark.parametrize('tensor_shape, slice_args', [
     ((8,), ['...']),
+    ((8,), [':']),
     ((8,), [slice(2, 4)]),
     ((8, 2), [slice(2, 4)]),
     ((8, 16), [':', slice(12, 14)]),
     ((2, 16, 16, 1), [':', slice(None, None, 2), slice(None, None, 2)]),
+    ((2, 16, 16, 1), [':', slice(1, None, 2), slice(None, None, 2)]),
+    ((2, 28, 42, 1), [':', slice(1, None, 2), slice(None, None, 2)]),
+    ((2, 1, 16, 16), ['...', slice(None, None, 2), slice(None, None, 2)]),
+    ((2, 1, 16, 16), [..., slice(None, None, 2), slice(None, None, 2)]),
+    ((2, 1, 16, 16), [Ellipsis, slice(None, None, 2), slice(None, None, 2)]),
 ])
 def test_slice_assign(tensor_shape, slice_args):
     original_tensor = np.random.normal(size=tensor_shape)
